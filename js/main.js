89ae7625632d978ca3494ee619d196d6d1580fac
visualer <api03025@gmail.com>
2018-04-08 00:04:28 +0800

    production update

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $.getScript(navigator.language.substr(0, 2).toLowerCase() !== "zh"
     console.log("MathJax load takes " + (timeStartup - timeLoad).toFixed(2));
 });
 
-let urlParams;
+let urlParams = {};
 (window.onpopstate = function () {
     let match,
         pl     = /\+/g,  // Regex for replacing addition symbol with a space
@@ -29,6 +29,121 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
     $('.selectpicker').selectpicker('mobile');
 }
 
+let $body = $("body");
+
+if (!es6SupportFlag && Cookies.get("dimmer") !== "shown") {
+  let $es6Dimmer = $(`
+    <div id="es6Dimmer" class="modal fade" role="dialog" style="top: 30%;" aria-hidden="true">
+    <div class="modal-dialog"><div class="modal-content"><div class="modal-body">
+    <h3 style="text-align: center;">
+    Your browser doesn't support ECMAScript 6 (ES2015), the program will work a bit slower.
+    <br /><br /> IE is no longer updated by Microsoft now. 
+    Using Edge, Chrome or Firefox will be better.</h3></div><div class="modal-footer">
+    <a type="button" href="https://www.google.com/chrome/" class="btn btn-primary">Download Chrome</a>
+    <a type="button" href="https://www.firefox.com.cn/" class="btn btn-primary">Download Firefox</a>
+    <a type="button" class="btn btn-default" data-dismiss="modal">Close</a>
+    </div></div></div></div>
+  `);
+  $es6Dimmer.appendTo("body").modal({backdrop: "static"});
+  Cookies.set("dimmer", "shown");
+}
+
+if ($body.hasClass("Step1")) {
+
+  let $selectType = $("#slType");
+  for (let i = 0; i < typeArr.length; i++)
+    $selectType.append(`<option value="${i}">${typeArr[i]}</option>`);
+
+  $selectType.selectpicker('val', 0)
+    .selectpicker('refresh')
+    .on("loaded.bs.select", clearTitle)
+    .on('changed.bs.select', clearTitle);
+
+} else if ($body.hasClass("Step2")) {
+
+  let type = parseInt(urlParams['Type']);
+  if (isNaN(type) || type > 5 || type < 0)
+    location.href = "Step1.html";
+
+  $("#title")[0].innerHTML += '<small>' + typeArr[type] + '</small>';
+  $("#dataTables").find("table")[type].style.display = "";
+
+  let $selectP1 = $("#slP1"), $selectP2 = $("#slP2"), $selectType = $("#slType");
+  let $selectP1P2 = $("#slP1, #slP2");
+
+  // generate options
+
+  for (let i = 0; i < p1Arr.length; i++) {
+    if (type >= 3) {
+      let threshold = (type === 3 ? 8 : 2);
+      if (i >= threshold) break;
+    }
+    $selectP1P2.append(`<option value="${i}">${p1Arr[i]}</option>`);
+  }
+  for (let i = 0; i < typeArr.length; i++)
+    $selectType.append(`<option value="${i}">${typeArr[i]}</option>`);
+
+  $selectType.selectpicker('val', type);
+  $(".selectpicker").selectpicker('refresh');
+
+  $("select").on("loaded.bs.select", clearTitle);
+  $selectP1.on('changed.bs.select', function (e, index) {
+    let i = index - 1;
+    clearTitle(); // only when slP1 changes the title have to be cleared; only active select
+    $selectP2.find("option").each(function () {
+      let value = parseInt($(this).val());
+      if (i % 2 === 0 && value === i + 1 || i % 2 === 1 && value === i - 1)
+        $selectP2.selectpicker('val', value);
+    });
+  });
+
+} else if ($body.hasClass("Step3")) {
+
+  // check integrity
+  for (let i of ['Type', 'P1', 'P2', 'Val1', 'Val2', 'RBM']) {
+    if (urlParams[i] === undefined) {
+      location.href = 'Step1.html';
+      break;
+    }
+  }
+
+  let params = {
+    type: parseInt(urlParams['Type']), // int
+    p1: parseInt(urlParams['P1']), // int
+    p2: parseInt(urlParams['P2']), // int
+    val1: urlParams['Val1'], // empty-able str
+    val2: urlParams['Val2'], // empty-able str
+    rbm: urlParams['RBM'] // empty-able str
+  };
+
+  let valueBetween = (xy, a, b) => (xy >= a || xy <= b);
+  let val1_ = parseFloat(params.val1), val2_ = parseFloat(params.val2), rbm_ = parseFloat(params.rbm);
+  let emptyNum = Number(isNaN(val1_)) + Number(isNaN(val2_)) + Number(isNaN(rbm_));
+  if (emptyNum >= 2 || isNaN(params.type) || isNaN(params.p1) || isNaN(params.p2) ||
+    !isNaN(val1_) && (val1_ < 0 || val1_ > 4) || !isNaN(val2_) && (val2_ < 0 || val2_ > 4) ||
+    !isNaN(rbm_) && (rbm_ < 30 || rbm_ > 500) || !valueBetween(params.type, 0, 5) ||
+    !valueBetween(params.p1, 0, 11) || !valueBetween(params.p2, 0, 11)
+  )
+    location.href = 'Step1.html';
+
+  // end validation
+
+  $("#title")[0].innerHTML += '<small>' + typeArr[params.type] + '</small>';
+
+  let plotParams = processOutput(getPlotParams(params));
+
+  $("#resultDiv")
+    .addClass(plotParams.ar !== AssignResult.error ? "alert-success" : "alert-danger")
+    .html(plotParams.resultString);
+  $("#yAxisLabel").html(plotParams.yAxisLabel);
+  $("#xAxisLabel").html(plotParams.xAxisLabel);
+  $("#" + plotParams.pointType + "Legend").css("display", "");
+  if (plotParams.bluePoint !== null)
+    $("#blueLegend").css("display", "");
+
+  $(drawPlot("plot-placeholder", plotParams));
+
+}
 
 
 
@@ -76,7 +191,7 @@ function changeEdit(mainP1, val1, val2) { // used in Step2
 
 function calculate(form) { // used in Step2
 
-    let n = parseInt(form.NCalc.value), m = parseInt(form.MCalc.value), t = type;
+    let n = parseInt(form.NCalc.value), m = parseInt(form.MCalc.value), t = parseInt(urlParams['Type']);
     if (n < m) [n, m] = [m, n];
     if (isNaN(n) || isNaN(m) || n <= 6 || m <= 0) return;
 
@@ -95,8 +210,8 @@ function calculate(form) { // used in Step2
                 <tr>
                     <td>\\(\\omega_\\mathrm{RBM}\\ ${isMetal(n, m) ? "(p=3)" : "(p=1,2)"} \\)</td>
                     <td>\\( ${rbmAS} \\ \\mathrm{cm^{-1}}\\)</td>
-                </tr>`
-            )
+                </tr>
+            `)
                 .click(() => editRBM(rbmAS))
                 .css("cursor", "pointer")
         );
